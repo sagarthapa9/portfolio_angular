0102dee3d51cf6d520353dfff100966af08a3fa3
Sagar Thapa <sagarthapa_9@yahoo.com>
2022-09-05 21:48:24 +0100

    added bootstap layout

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
